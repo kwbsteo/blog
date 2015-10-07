cbff378ad8b94210758e0f527d3d8d752de57e81
kweebin@gmail.com <kweebin@gmail.com>
2015-10-06 19:10:40 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'defcdf928569ff09978085491a128780f7b75a7049f7ad530774c9582d20709c6f84c96e4e2c74c045c4d90c340145f3c20b5fa42e25ad1fe2712803877b6b8e'
